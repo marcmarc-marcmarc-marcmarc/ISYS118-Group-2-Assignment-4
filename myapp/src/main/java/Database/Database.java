bb4e85342a03b9d40152cb90e37fd5d99f50e6c8
BenRuggles <s4107052@student.rmit.edu.au>
2026-02-09 20:00:55 +1100

    createPerson skeleton added. Need a function to convert person objects to a single line prior to finishing this..

--- myapp/src/main/java/Database/Database.java
+++ myapp/src/main/java/Database/Database.java
@@ -2,17 +2,29 @@ package Database;
 
 import java.io.File;
 import java.io.IOException;
+import DigitalID.Person;
 
 public class Database {
-    // final Vars
+    // Final Vars
     public static final String DB_FILE = "../database.txt";
     public static final String DELIMITER = "|";
     
-    // Check if DB exists
-    private static void checkDatabaseExists() throws IOException {
+    // Add Person Object to the Database
+    private static void createPerson(Person person) throws IOException {
+        ensureDatabaseExists(); // Create DB if necessary
+
+        // Ensure personID doesn't already exist
+
+        // Write person to new line
+    }
+
+
+    // Helper function to check if DB exists
+    private static void ensureDatabaseExists() throws IOException {
         File file = new File(DB_FILE);
         if (!file.exists()) {
             file.createNewFile();
         }
     }
+
 }
